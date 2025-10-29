e161fa75a86ce93116b0319a7ec4598a8ac0f8c2
Yu Shi <shiyuqm@gmail.com>
2025-10-29 18:19:08 -0400

    Enable pqsl

--- IdeaProjects/SpringBoot/src/main/java/com/practice/MyController.java
+++ IdeaProjects/SpringBoot/src/main/java/com/practice/MyController.java
@@ -1,19 +1,23 @@
 package com.practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RestController
 @RequestMapping("/test")
 public class MyController {
 
-    // GET: http://localhost:8080/api/hello?name=Chelsea
-    // GET: http://localhost:8080/api/hello   => "Hello, World!" by default
+    // GET: http://localhost:8080/test/hello?name=Chelsea
+    // GET: http://localhost:8080/test/hello   => "Hello, World!" by default
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "World") String name) {
         return "Hello, " + name + "!";
     }
 
-    // POST: http://localhost:8080/api/echo
+    // POST: http://localhost:8080/test/echo
     // Body -> raw (JSON) -> { "msg": "Hello World." }
     @PostMapping("/echo")
     public Msg echo(@RequestBody Msg input) {
@@ -26,6 +30,19 @@ public class MyController {
         public Msg() {}
         public Msg(String msg) {
             this.msg = msg;
+        }
+    }
+
+    // test postgreSQL
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/psql")
+    public String testDbConnection() {
+        try (Connection conn = dataSource.getConnection()) {
+            return "[DB OK] Connected to " + conn.getMetaData().getDatabaseProductName();
+        } catch (Exception e) {
+            return "[DB ERROR] " + e.getMessage();
         }
     }
 }
