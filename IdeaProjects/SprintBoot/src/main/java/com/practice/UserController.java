aef8060139dede3d62899f492e9e0324cb889c43
Yu Shi <shiyuqm@gmail.com>
2025-10-29 11:22:03 -0400

    Enable actuator

---
+++ IdeaProjects/SprintBoot/src/main/java/com/practice/UserController.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    // read UserId
+    // insert (RequestBody)
+    // delete UserId
+    // Update UserId + new RequestBody user
+}
