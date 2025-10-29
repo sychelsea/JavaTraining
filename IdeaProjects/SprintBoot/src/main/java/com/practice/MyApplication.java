63b04ed85cf08a7ca0ca3e185f31caee6237b0ab
Yu Shi <shiyuqm@gmail.com>
2025-10-29 17:30:04 -0400

    Documentation is enabled via Swagger

--- IdeaProjects/SprintBoot/src/main/java/com/practice/MyApplication.java
+++ IdeaProjects/SprintBoot/src/main/java/com/practice/MyApplication.java
@@ -3,6 +3,12 @@ package com.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Swagger/OpenAPI enabled
+ * <a href="http://localhost:9091/swagger-ui/index.html">Swagger doc</a>
+ * <a href="http://localhost:9091/v3/api-docs">OpenAPI link</a>
+ */
 @SpringBootApplication
 public class MyApplication {
     public static void main(String[] args) {
